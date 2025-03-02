fe2bbc2bfcadde7331a0a0ef226f54063186980f
Vladislav <vlad.4sa@gmail.com>
2025-03-02 22:42:35 +0700

    4.1. Добавлен компонент баннера.

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -1,14 +1,13 @@
+import AsideBanner from '../../components/aside-banner/aside-banner';
+
+
 export default function CatalogPage(): JSX.Element {
   return (
     <section className="catalog">
       <div className="container">
-
         <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
         <div className="page-content__columns">
-
-          <div className="catalog__aside">
-            <img src="img/banner.png" />
-          </div>
+          <AsideBanner />
 
           <div className="catalog__content">
             <div className="cards catalog__cards">
