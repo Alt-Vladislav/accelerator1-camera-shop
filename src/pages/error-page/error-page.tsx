116db0dd0db89d80aa0a54fa24ec218ab0f45d41
Vladislav <vlad.4sa@gmail.com>
2025-03-11 17:04:39 +0700

    4.5. Добавлена страница 404.

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -1,5 +1,29 @@
-export default function ErrorPage(): JSX.Element {
+import { AppRoute } from '../../consts';
+import { Link } from 'react-router-dom';
+import './error-page.css';
+
+type ErrorPageProps = {
+  isButtonHidden?: boolean;
+};
+
+export default function ErrorPage({ isButtonHidden = false }: ErrorPageProps): JSX.Element {
   return (
-    <h1>Blank</h1>
+    <>
+      <h1 className="title--h3 link-container">404 Ресурс не найден</h1>
+      <section className="error-container">
+        <span className="four"><span className="screen-reader-text">4</span></span>
+        <span className="zero"><span className="screen-reader-text">0</span></span>
+        <span className="four"><span className="screen-reader-text">4</span></span>
+      </section>
+      {
+        isButtonHidden || (
+          <div className="link-container">
+            <Link to={AppRoute.Catalog.Path} className="btn btn--purple product-card__btn">
+              На главную
+            </Link>
+          </div>
+        )
+      }
+    </>
   );
 }
