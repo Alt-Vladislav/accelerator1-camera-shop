432556c7d64661931827bc44f73045bd0653d3c8
Vladislav <vlad.4sa@gmail.com>
2025-03-16 21:14:18 +0700

    6.4. Компонент навигации.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import NavigationList from '../navigation-list/navigation-list';
 import { Page } from '../../types';
 import Logo from '../logo/logo';
 
@@ -12,22 +13,9 @@ export default function Header({ currentPage }: HeaderProps): JSX.Element {
       <div className="container">
         <Logo location='header' isUpButton={currentPage === 'Catalog'} />
         <nav className="main-nav header__main-nav">
-          <ul className="main-nav__list">
-            <li className="main-nav__item">
-              <a className="main-nav__link" href="catalog.html">Каталог</a>
-            </li>
-            <li className="main-nav__item">
-              <a className="main-nav__link" href="#">Гарантии</a>
-            </li>
-            <li className="main-nav__item">
-              <a className="main-nav__link" href="#">Доставка</a>
-            </li>
-            <li className="main-nav__item">
-              <a className="main-nav__link" href="#">О компании</a>
-            </li>
-          </ul>
+          <NavigationList currentPage={currentPage} type='Header' />
         </nav>
       </div>
-    </header>
+    </header >
   );
 }
